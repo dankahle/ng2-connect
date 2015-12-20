04c7449b501dc2075c2859cb5cc5a4c4bf393cca
Dan Kahle <Daniel.Kahle@acxiom.com>
2015-12-20 14:21:01 -0600

    added hero

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,4 @@
 import {bootstrap, Component, EventEmitter, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/core';
-import {HeroService} from './hero/heroService';
-import {Hero} from './hero/hero'
 import {Http, HTTP_PROVIDERS} from 'angular2/http';
 
 
@@ -11,38 +9,10 @@ import {Http, HTTP_PROVIDERS} from 'angular2/http';
 })
 
 export class AppComponent {
-   heroes:Hero[] = [];
-
-   constructor(hs:HeroService) {
-
-/*
-      var obs = {
-         next(x) { console.log(x, 'lala')}, // gets called, but not onNext
-         throw(x) { console.log('error', x)},
-         return() { console.log('completed')},// never gets called
-      }
-*/
-
-/*
-      hs
-         .getHeroes()
-         .subscribe(x => {
-            console.log(x)
-            this.heroes.push(x)
-         });
-*/
-      hs
-         .getHeroes()
-         .subscribe(x => {
-            //console.log(x.json())
-            this.heroes = x.json()
-         });
-
-   }
-
 
 }
-bootstrap(AppComponent, [Http, HTTP_PROVIDERS, HeroService]);
+
+bootstrap(AppComponent, [Http, HTTP_PROVIDERS]);
 
 
 
