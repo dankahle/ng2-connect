b97968dc793d111061c884c6564c4e355b79b5f0
Dan Kahle <Daniel.Kahle@acxiom.com>
2015-12-21 18:05:56 -0600

    heroForm

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,20 @@
-import {bootstrap, Component, EventEmitter, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/core';
+import {bootstrap, Component, CORE_DIRECTIVES, FORM_DIRECTIVES, FORM_PROVIDERS} from 'angular2/core';
 import {Http, HTTP_PROVIDERS} from 'angular2/http';
-
+import  {Hero} from './hero-form/hero';
+import  {HeroForm} from './hero-form/hero-form';
 
 @Component({
    selector: 'my-app',
-   templateUrl: 'app/app.html',
-   directives: [CORE_DIRECTIVES, FORM_DIRECTIVES],
+   //templateUrl: 'app/app.html',
+   template: '<hero-form></hero-form>',
+   directives: [CORE_DIRECTIVES, FORM_DIRECTIVES, HeroForm],
 })
 
 export class AppComponent {
 
 }
 
-bootstrap(AppComponent, [Http, HTTP_PROVIDERS]);
+bootstrap(AppComponent, [Http, HTTP_PROVIDERS, FORM_PROVIDERS]);
 
 
 
